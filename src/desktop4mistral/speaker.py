74580c9cb31bf98a38a7719efa2c5ef67276d4a1
Hathibelagal <42626106+hathibelagal-dev@users.noreply.github.com>
2025-03-26 03:58:41 +0100

    fix kokoro speech

--- src/desktop4mistral/speaker.py
+++ src/desktop4mistral/speaker.py
@@ -3,6 +3,7 @@ from str2speech.speaker import Speaker as S
 import tempfile
 import scipy.io.wavfile as wav
 import sounddevice as sd
+import os
 
 class Speaker:
     def __init__(self):
@@ -13,7 +14,10 @@ class Speaker:
             tfile = tempfile.NamedTemporaryFile(delete=False, suffix=".wav")
             print(tfile.name)
             self.speaker.text_to_speech(text, tfile.name)
-            sample_rate, data = wav.read(tfile.name)
+            dir_path = os.path.dirname(os.path.realpath(tfile.name))
+            file_name = os.path.basename(tfile.name)
+            file_name = "0_" + file_name
+            sample_rate, data = wav.read(os.path.join(dir_path, file_name))
             sd.play(data, sample_rate)
             tfile.close()
         
