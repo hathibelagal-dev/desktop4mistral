2357151ee6a0eed247efb88a1812a59804229a94
Hathibelagal <42626106+hathibelagal-dev@users.noreply.github.com>
2025-03-27 16:49:02 +0100

    improved tts

--- src/desktop4mistral/speaker.py
+++ src/desktop4mistral/speaker.py
@@ -16,8 +16,14 @@ class Speaker:
             self.speaker.text_to_speech(text, tfile.name)
             dir_path = os.path.dirname(os.path.realpath(tfile.name))
             file_name = os.path.basename(tfile.name)
-            file_name = "0_" + file_name
-            sample_rate, data = wav.read(os.path.join(dir_path, file_name))
-            sd.play(data, sample_rate)
+            i = 0            
+            while True:                
+                file_name = f"{i}_" + file_name
+                if not os.path.exists(os.path.join(dir_path, file_name)):
+                    break
+                sample_rate, data = wav.read(os.path.join(dir_path, file_name))
+                sd.play(data, sample_rate, blocking=True)
+                i += 1
+                
             tfile.close()
         
